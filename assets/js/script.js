e0a8ef9edf9a910289a87e5f5faa7a36d38523f3
Abin Justin Kumaravel <abinjustinkumaravel@gmail.com>
2024-09-22 08:07:06 +0000

    nav bar text color

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,18 @@
 
 
 
-/**
- * PRELOAD
- * 
- * loading will be end after document is loaded
- */
+// /**
+//  * PRELOAD
+//  * 
+//  * loading will be end after document is loaded
+//  */
 
-const preloader = document.querySelector("[data-preaload]");
+// const preloader = document.querySelector("[data-preaload]");
 
-window.addEventListener("load", function () {
-  preloader.classList.add("loaded");
-  document.body.classList.add("loaded");
-});
+// window.addEventListener("load", function () {
+//   preloader.classList.add("loaded");
+//   document.body.classList.add("loaded");
+// });
 
 
 
